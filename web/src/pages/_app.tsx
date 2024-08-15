202b2685a73bbbaabc9936682f11599784c4fddf
Progress Ochuko Eyaadah <devkoxy@gmail.com>
2024-08-15 10:28:06 +0100

    set up project

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
+import WalletContextProvider from "@/context/WalletContextProvider.jsx";
+import TransitionContextProvider from "@/context/TransitionContextProvider.jsx";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useWallet } from "@solana/wallet-adapter-react";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <WalletContextProvider>
+
+        <ToastContainer />
+
+        <TransitionContextProvider>
+          <Component {...pageProps} />
+        </TransitionContextProvider>
+      </WalletContextProvider>
+    </>
+  );
 }
