99fa3688b9eb8bd9e252b267e6f91c5222dc31d2
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-09-18 05:51:41 +0100

    Create Upcoming as a component

--- web/src/pages/dashboard/Profile/index.js
+++ web/src/pages/dashboard/Profile/index.js
@@ -1,6 +1,7 @@
 import Layout from "../layout";
 import Image from "next/image";
 import Link from "next/link";
+import MatchCard from "../../../component/MatchCard.jsx";
 import NavbarSection from "../../../component/navbar.jsx";
 import User from "../../../../public/images/user.png";
 import ManU from "../../../../public/images/image 16.png";
@@ -65,54 +66,66 @@ export default function Profile() {
 
         <div className="text-white col-span-2">
           <h1 className="font-medium text-xl mt-5">Upcoming Football Game</h1>
-          <div className="border border-white rounded-xl bg-[#330034] mt-5 md:mt-10">
-            <div className="flex justify-between gap-10 px-5 py-3">
-              <h1 className="text-sm md:text-md font-medium">
-                UEFA Champions League
-              </h1>
-              <h1> Oct 15</h1>
-            </div>
-            <div className="flex justify-between items-center gap-10 px-5 py-3 bg-[#170016]">
-              <div className="flex items-center gap-2">
-                <Image src={ManU} alt={ManU} />
-                <h1>Man United</h1>
+          {/* <div className="border border-white rounded-xl bg-[#330034] mt-5 md:mt-10">
+              <div className="flex justify-between gap-10 px-5 py-3">
+                <h1 className="text-sm md:text-md font-medium">
+                  UEFA Champions League
+                </h1>
+                <h1> Oct 15</h1>
               </div>
-              <div>Vs</div>
-              <div className="flex items-center gap-2">
-                <Image src={ManCity} alt={ManCity} />
-                <h1>Man City</h1>
+              <div className="flex justify-between items-center gap-10 px-5 py-3 bg-[#170016]">
+                <div className="flex items-center gap-2">
+                  <Image src={ManU} alt={ManU} />
+                  <h1>Man United</h1>
+                </div>
+                <div>Vs</div>
+                <div className="flex items-center gap-2">
+                  <Image src={ManCity} alt={ManCity} />
+                  <h1>Man City</h1>
+                </div>
               </div>
-            </div>
-            <div className="flex justify-between gap-10 px-5 py-3">
-              <h1 className="text-sm md:text-md font-medium">Match Winner</h1>
-            </div>
-            <div className="flex justify-center gap-10 px-5 py-3 mb-3 bg-[#170016]">
-              <div className="flex items-center gap-2">
-                <Link href="#">
-                  <div className="bg-[#604360] rounded-lg text-center border border-white py-1 px-3">
-                    <p className="text-sm md:text-md font-light">1</p>
-                    <h1 className="text-sm md:text-md font-medium">6.50</h1>
-                  </div>
-                </Link>
+              <div className="flex justify-between gap-10 px-5 py-3">
+                <h1 className="text-sm md:text-md font-medium">Match Winner</h1>
               </div>
-              <div className="flex items-center gap-2">
-                <Link href="#">
-                  <div className="bg-[#604360] rounded-lg text-center border border-white py-1 px-3">
-                    <p className="text-sm md:text-md font-light">X</p>
-                    <h1 className="text-sm md:text-md font-medium">4.00</h1>
-                  </div>
-                </Link>
+              <div className="flex justify-center gap-10 px-5 py-3 mb-3 bg-[#170016]">
+                <div className="flex items-center gap-2">
+                  <Link href="#">
+                    <div className="bg-[#604360] rounded-lg text-center border border-white py-1 px-3">
+                      <p className="text-sm md:text-md font-light">1</p>
+                      <h1 className="text-sm md:text-md font-medium">6.50</h1>
+                    </div>
+                  </Link>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Link href="#">
+                    <div className="bg-[#604360] rounded-lg text-center border border-white py-1 px-3">
+                      <p className="text-sm md:text-md font-light">X</p>
+                      <h1 className="text-sm md:text-md font-medium">4.00</h1>
+                    </div>
+                  </Link>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Link href="#">
+                    <div className="bg-[#604360] rounded-lg text-center border border-white py-1 px-3">
+                      <p className="text-sm md:text-md font-light">2</p>
+                      <h1 className="text-sm md:text-md font-medium">1.40</h1>
+                    </div>
+                  </Link>
+                </div>
               </div>
-              <div className="flex items-center gap-2">
-                <Link href="#">
-                  <div className="bg-[#604360] rounded-lg text-center border border-white py-1 px-3">
-                    <p className="text-sm md:text-md font-light">2</p>
-                    <h1 className="text-sm md:text-md font-medium">1.40</h1>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
+            </div> */}
+          <MatchCard
+            team1Name="Man United"
+            team1Image={ManU}
+            team2Name="Man City"
+            team2Image={ManCity}
+            matchDate="Oct 15"
+            odds={{
+              1: "6.50", // Man United win
+              X: "4.00", // Draw
+              2: "1.40", // Man City win
+            }}
+          />
         </div>
       </div>
     </Layout>
