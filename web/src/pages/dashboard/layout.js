ef5b8905b8d9813aecff4b1889eb29f6bc963b67
OutstandingVick <scottdodo2015@outlook.com>
2024-09-23 08:58:37 +0100

    completed my task

--- web/src/pages/dashboard/layout.js
+++ web/src/pages/dashboard/layout.js
@@ -1,18 +1,12 @@
-import SideNav from "./sidenav";
+import SideNav from './sidenav';
 
-
- 
 export default function Layout({ children }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-     
-      <div className="w-full flex-none md:w-64">
-        <SideNav /> 
+    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden bg-back'>
+      <div className='w-full flex-none md:w-80'>
+        <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}
-     
-      </div>
-    
+      <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
     </div>
   );
 }
