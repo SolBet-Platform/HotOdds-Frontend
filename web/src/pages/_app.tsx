576094bc0fbbf382611438f529cb334e4675b23b
koxy <devkoxy@gmail.com>
2024-09-26 16:46:05 +0100

    more bugs

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,20 +2,14 @@ import '@/styles/globals.css';
 import WalletContextProvider from '@/context/WalletContextProvider.jsx';
 import TransitionContextProvider from '@/context/TransitionContextProvider.jsx';
 import type { AppProps } from 'next/app';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useWallet } from '@solana/wallet-adapter-react';
 import Footer from '@/component/Footer';
-import NavbarSection from '@/component/Navbar';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <WalletContextProvider>
-        <ToastContainer />
-
         <TransitionContextProvider>
-          {/* <NavbarSection /> */}
           <Component {...pageProps} />
           <Footer />
         </TransitionContextProvider>
